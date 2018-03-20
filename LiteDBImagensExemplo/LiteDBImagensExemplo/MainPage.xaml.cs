631fd4abfb84bad31dcf405de1e0df09d706e5ef
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-03-20 11:28:04 -0300

    Artigo

--- LiteDBImagensExemplo/LiteDBImagensExemplo/MainPage.xaml.cs
+++ LiteDBImagensExemplo/LiteDBImagensExemplo/MainPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using LiteDB;
+using LiteDBImagensExemplo.Helpers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,9 +13,55 @@ namespace LiteDBImagensExemplo
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        LiteDatabase _dataBase;
+        string identificador = "idImagem";
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            _dataBase = new LiteDatabase(DependencyService.Get<IFileHelper>().GetLocalFilePath("MeuBanco.db"));
+
+            if (_dataBase.FileStorage.Exists(identificador))
+            {
+                Stream stream = _dataBase.FileStorage.OpenRead(identificador);
+
+                imgExibir.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+
+        private void btnSalvar_Clicked(object sender, EventArgs e)
+        {
+
+            //Url para gravar a imagem
+            Stream stream = GetImageStreamFromUrl("");
+
+            //Se existir a Stream
+            if (stream != null)
+            {
+                //Verfica se ja existe a imagem,se existir apaga
+                if (_dataBase.FileStorage.Exists(identificador))
+                {
+                    _dataBase.FileStorage.Delete(identificador);
+                }
+                _dataBase.FileStorage.Upload(identificador, "Teste", stream);
+            }
+
+        }
+
+        //Método para transformar a imagem da URL em Stream
+        private  Stream GetImageStreamFromUrl(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    Stream stream = new MemoryStream(imageBytes);
+                    return stream;
+                }
+            }
+            return null;
+        }
+    }
 }
